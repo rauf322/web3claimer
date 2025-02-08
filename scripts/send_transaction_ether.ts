743a30401727922e16123210943b7b793060ebd3
rauffaizov <rauf.faizov.200271@gmail.com>
2025-02-08 18:34:35 +0100

    remove TxData to have less variables

--- scripts/send_transaction_ether.ts
+++ scripts/send_transaction_ether.ts
@@ -20,18 +20,18 @@ async function get_balance(address: any, provider: any){
     const balance = await provider.getBalance(await address.getAddress())
     return balance
 }
-
+//parseEther - convert ether to wei
+//formatEther - convert wei to ether
 async function send_ethereum(ADDRESS1: any, ADDRESS2: any, provider: any){
     const addressBalance1 = await get_balance(ADDRESS1, provider)
     const addressBalance2 = await get_balance(ADDRESS2, provider)
     const minBalanceRequired = ethers.parseEther("0.0001");
     const amountOfSend = addressBalance2 - minBalanceRequired;
     console.log(amountOfSend)
-    const txData = {
+    const tx = await ADDRESS2.sendTransaction({
         to:(await ADDRESS1.getAddress()),
         value: amountOfSend
-    }
-    const tx = await ADDRESS2.sendTransaction(txData)
+    })
     await tx.wait()
     console.log(`Transaction hash: ${tx.hash}`)
     console.log(`Balance of address_1: ${ethers.formatEther(await get_balance(ADDRESS1, provider))}`)
