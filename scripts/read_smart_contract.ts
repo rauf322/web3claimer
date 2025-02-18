f4e2479af96fbd9fd157feb3f3129e360f808269
rauffaizov <rauf.faizov.200271@gmail.com>
2025-02-18 19:49:00 +0100

    improve typing plus logic of claiming

--- scripts/read_smart_contract.ts
+++ scripts/read_smart_contract.ts
@@ -1,5 +1,6 @@
 //Get the balance of different not connected with signer wallets 
 
+import { HardhatEthersSigner } from "@nomicfoundation/hardhat-ethers/signers";
 import hre, { ethers } from "hardhat";
 
 
@@ -15,60 +16,104 @@ const ERC20_ABI = [
     //veCarv claim
     "function multicall(bytes[]) public",
     //veCarv withdraw 
-    "function withdraw(uint256 amount, uint256 duration) public"
+    "function withdraw(uint256 amount, uint256 duration) public",
+    //xai transfer
+    "function transfer(address to, uint256 amount) public"
 ]
 
 async function main(){
-    console.log("Connect to provide(RPC) started...")
-    const provider = ethers.provider;
-    const signer = await ethers.getSigners()
-    await get_esxai(signer[2])
-    await redeem_esxai(signer[2])
-    await complete_redemption(signer[2])
-    await claim_veCarv(signer[2])
-    await redeem_veCarv(signer[2])
+    for (let i = 0; i < 1; i++){
+        try{
+            console.log("Connect to provide(RPC) started...")
+            const provider = ethers.provider;
+            const signers:HardhatEthersSigner[] = await ethers.getSigners()
+            const signer:HardhatEthersSigner = signers[2]
+            await claim_esxai(signer)
+            await redeem_esxai(signer)
+            await claim_xai(signer)
+            await claim_veCarv(signer)
+            await redeem_veCarv(signer)
+        }catch(e){
+            console.log("Error at main ❌😵❌")
+        }
+    }
+
 }
 
-async function claim_veCarv(address:any){
-    const contract = new ethers.Contract("0xa91fF8b606BA57D8c6638Dd8CF3FC7eB15a9c634", ERC20_ABI, address);
-    const tx = await contract.multicall(["0xac9650d800000000000000000000000000000000000000000000000000000000000000200000000000000000000000000000000000000000000000000000000000000001000000000000000000000000000000000000000000000000000000000000002000000000000000000000000000000000000000000000000000000000000000240962ef79000000000000000000000000000000000000000000000000000000000000122300000000000000000000000000000000000000000000000000000000"])
-    await tx.wait()
-    console.log(tx)
+async function claim_veCarv(address:HardhatEthersSigner){
+    try{
+        const contract = new ethers.Contract("0xa91fF8b606BA57D8c6638Dd8CF3FC7eB15a9c634", ERC20_ABI, address);
+        const tx = await contract.multicall(["0xac9650d800000000000000000000000000000000000000000000000000000000000000200000000000000000000000000000000000000000000000000000000000000001000000000000000000000000000000000000000000000000000000000000002000000000000000000000000000000000000000000000000000000000000000240962ef79000000000000000000000000000000000000000000000000000000000000122300000000000000000000000000000000000000000000000000000000"])
+        await tx.wait()
+        console.log("Successfully claimed veCarv ✅")
+    } catch(e){
+        console.log("Error at claim veCarv ❌😵❌")
+    }
 }
 
-async function redeem_veCarv(address:any){
-    const contract = new ethers.Contract("0x2b790Dea1f6c5d72D5C60aF0F9CD6834374a964B",ERC20_ABI, address);
-    const amount = await contract.balanceOf(address.address)
-    const duration = 12960000;
-    const tx = await contract.withdraw(amount,duration)
-    await tx.wait()
-    console.log(`Successfully redeemed veCarv ${amount}`)
+async function redeem_veCarv(address:HardhatEthersSigner){
+    try{
+        const contract = new ethers.Contract("0x2b790Dea1f6c5d72D5C60aF0F9CD6834374a964B",ERC20_ABI, address);
+        const amount = await contract.balanceOf(address.address)
+        if(Number(ethers.formatEther(amount)) > 100){
+            const duration = 12960000;
+            const tx = await contract.withdraw(amount,duration)
+            await tx.wait()
+            console.log(`Successfully redeemed veCarv ${amount} ✅`)
+        }else{
+            console.log(`Not enough veCarv to redeem ${ethers.formatEther(amount)} 😿`)
+        }
+    }catch(e){
+        console.log("Error at redeem veCarv ❌😵❌")
+    }
 }
 
 
-async function get_esxai(address:any){
-    const contract = new ethers.Contract("0xF9E08660223E2dbb1c0b28c82942aB6B5E38b8E5", ERC20_ABI, address)
-    const tx = await contract.claimFromPools(["0x7b9F49fc73C380E13a0bDCf91B53C0AE612Df8BF","0x76B1121F1861e38a290eC980143149D5695B9997"])
-    await tx.wait()
-    // console.log(tx)
+async function claim_esxai(address:HardhatEthersSigner){
+    try{
+        const contract = new ethers.Contract("0xF9E08660223E2dbb1c0b28c82942aB6B5E38b8E5", ERC20_ABI, address)
+        const tx = await contract.claimFromPools(["0x7b9F49fc73C380E13a0bDCf91B53C0AE612Df8BF","0x76B1121F1861e38a290eC980143149D5695B9997"])
+        await tx.wait()
+        console.log(`Successfully claimed esxai ✅`)
+    }catch(e){
+        console.log("Error: at claim_esxai ❌😵❌")
+    }
 }
 
-async function redeem_esxai(address:any){
-    const contract = new ethers.Contract("0x4C749d097832DE2FEcc989ce18fDc5f1BD76700c", ERC20_ABI, address)
-    const amount = await contract.balanceOf(address.address)
-    const duration = 15552000; // This is fine since it's small
-    const tx = await contract.startRedemption(amount, duration);
-    await tx.wait()
-    // console.log(tx)
-    console.log(`Successfully redeemed esxai ${amount}`)
+async function redeem_esxai(address:HardhatEthersSigner){
+    try{
+        const contract = new ethers.Contract("0x4C749d097832DE2FEcc989ce18fDc5f1BD76700c", ERC20_ABI, address)
+        const amount = await contract.balanceOf(address.address)
+        if (Number(ethers.formatEther(amount)) >= 100){
+            const duration = 15552000;
+            const tx = await contract.startRedemption(amount, duration);
+            await tx.wait()
+            // console.log(tx)
+            console.log(`Successfully redeemed esxai ${amount} ✅`)
+        }else{
+            console.log(`Not enough esxai to redeem ${ethers.formatEther(amount)}😿`)
+        }
+    }catch(e){
+        console.log("Error at redeem esxai ❌😵❌")
+    }
+
 }
 
-async function complete_redemption(address:any){
-    const contract = new ethers.Contract("0x4C749d097832DE2FEcc989ce18fDc5f1BD76700c", ERC20_ABI, address)
-    const tx = await contract.completeRedemption(69)
-    tx.wait()
-    console.log(tx)
+async function claim_xai(address:HardhatEthersSigner){
+    try{
+        let contract = new ethers.Contract("0x4C749d097832DE2FEcc989ce18fDc5f1BD76700c", ERC20_ABI, address)
+        const tx = await contract.completeRedemption(71)
+        tx.wait()
+        contract = new ethers.Contract("0x4Cb9a7AE498CEDcBb5EAe9f25736aE7d428C9D66", ERC20_ABI, address)
+        let balance = await contract.balanceOf(address.address)
+        const tx_transfer = await contract.transfer("0xbe58fAE5B38cA7092Ea7D010d8Fd62295dB126D8", balance)
+        tx_transfer.wait()
+        console.log(`Balance after: ${ethers.formatEther(balance)} ✅`)
+    }catch(e){
+        console.log("Error at claim xai ❌😵❌")
+    }
 }
+
 
 main()
     .then(() => process.exit(0))
